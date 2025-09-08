37bd3a8915b32570622e900f686e5af658bb31d6
Eno <enoyoo02@gmail.com>
2025-09-08 18:06:18 -0400

    Initial Commit

---
+++ Old Phone Pub/Old_Phone_PubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Old_Phone_PubApp.swift
+//  Old Phone Pub
+//
+//  Created by Eno Yoo on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Old_Phone_PubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
